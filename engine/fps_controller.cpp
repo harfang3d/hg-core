d4a4123fa084d2c4d30691a1ed9c0fa0e593d1e4
HARFANG 3D (admin) <assemble3d@gmail.com>
2022-06-30 23:54:47 +0200

    Functional minimal rendering test.

---
+++ engine/fps_controller.cpp
@@ -0,0 +1,34 @@
+// HARFANG(R) Copyright (C) 2021 Emmanuel Julien, NWNC HARFANG. Released under GPL/LGPL/Commercial Licence, see licence.txt for details.
+
+#include "foundation/math.h"
+#include "foundation/matrix3.h"
+#include "foundation/time.h"
+#include "foundation/vector3.h"
+
+namespace hg {
+
+void FpsController(bool key_up, bool key_down, bool key_left, bool key_right, bool btn, float dx, float dy, Vec3 &pos, Vec3 &rot, float speed, time_ns dt_t) {
+	const auto world = RotationMat3(rot.x, rot.y, rot.z);
+	const auto right = GetX(world), front = GetZ(world);
+
+	Vec3 dt{};
+
+	if (key_up)
+		dt += front;
+	else if (key_down)
+		dt -= front;
+
+	if (key_left)
+		dt -= right;
+	else if (key_right)
+		dt += right;
+
+	pos += dt * speed * time_to_sec_f(dt_t);
+
+	if (btn) {
+		rot += {-dy * 0.005f, dx * 0.005f, 0.f};
+		rot.x = Wrap(rot.x, -Pi, Pi);
+	}
+}
+
+} // namespace hg
--- tests/test_render_pipeline.cpp
+++ tests/test_render_pipeline.cpp
@@ -8,28 +8,84 @@
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 
+#include "foundation/matrix44.h"
 #include "foundation/time.h"
+#include "foundation/vector3.h"
+#include "foundation/projection.h"
+
+#include "engine/fps_controller.h"
 
 using namespace hg;
 
-void test_init() {
+//----------------------------------------------
+
+bool RenderInit(GLFWwindow *win) {
+	sg_desc desc;
+	memset(&desc, 0, sizeof(sg_desc));
+	sg_setup(&desc);
+
+	return true;
 }
 
-void test_init_ref() {
-	// create window and GL context via GLFW
+GLFWwindow *RenderInit(int width, int height, const std::string &title) {
 	glfwInit();
+
+#if defined(SOKOL_GLCORE33) || defined(SOKOL_GLES2) || defined(SOKOL_GLES3) // any GL
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	GLFWwindow *w = glfwCreateWindow(640, 480, "Sokol Triangle GLFW", 0, 0);
-	glfwMakeContextCurrent(w);
+	GLFWwindow *win = glfwCreateWindow(640, 480, title.c_str(), 0, 0);
+	glfwMakeContextCurrent(win);
 	glfwSwapInterval(1);
+#else
+#error "Unsupported graphic API"
+#endif
 
-	// setup sokol_gfx
-	sg_desc desc;
-	memset(&desc, 0, sizeof(sg_desc));
-	sg_setup(&desc);
+	RenderInit(win);
+	return win;
+}
+
+void RenderShutdown() {
+	sg_shutdown();
+	glfwTerminate();
+}
+
+/*
+void SetView2D(bgfx::ViewId id, int x, int y, int res_x, int res_y, float znear = -1.f, float zfar = 1.f,
+	uint16_t clear_flags = BGFX_CLEAR_COLOR | BGFX_CLEAR_DEPTH, const Color &clear_color = Color::Black, float depth = 1.f, uint8_t stencil = 0,
+	bool y_up = false);
+void SetViewPerspective(bgfx::ViewId id, int x, int y, int res_x, int res_y, const Mat4 &world, float znear = 0.01f, float zfar = 1000.f,
+	float zoom_factor = 1.8, uint16_t clear_flags = BGFX_CLEAR_COLOR | BGFX_CLEAR_DEPTH, const Color &clear_color = Color::Black, float depth = 1.f,
+	uint8_t stencil = 0, const Vec2 &aspect_ratio = {});
+void SetViewOrthographic(bgfx::ViewId id, int x, int y, int res_x, int res_y, const Mat4 &world, float znear = 0.01f, float zfar = 1000.f, float size = 1.f,
+	uint16_t clear_flags = BGFX_CLEAR_COLOR | BGFX_CLEAR_DEPTH, const Color &clear_color = Color::Black, float depth = 1.f, uint8_t stencil = 0,
+	const Vec2 &aspect_ratio = {});
+*/
+
+//----------------------------------------------
+static bool key_up = false, key_down = false, key_left = false, key_right = false;
+
+static void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods) {
+	bool v = false;
+	if (action == GLFW_PRESS || action == GLFW_REPEAT)
+		v = true;
+
+	if (key == GLFW_KEY_W)
+		key_up = v;
+	if (key == GLFW_KEY_S)
+		key_down = v;
+	if (key == GLFW_KEY_A)
+		key_left = v;
+	if (key == GLFW_KEY_D)
+		key_right = v;
+}
+
+//
+void test_init() {
+	GLFWwindow *win = RenderInit(640, 480, "Sokol Triangle GLFW");
+
+	glfwSetKeyCallback(win, key_callback);
 
 	// a vertex buffer (position/color)
 	const float vertices[] = {0.0f, 0.5f, 0.5f, 1.0f, 0.0f, 0.0f, 1.0f, 0.5f, -0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 1.0f, -0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 1.0f};
@@ -42,20 +98,31 @@ void test_init_ref() {
 	// a shader
 	sg_shader_desc shader_desc;
 
+	typedef struct {
+		Mat44 mvp;
+	} shader_vs_params;
+
 	memset(&shader_desc, 0, sizeof(sg_shader_desc));
+
 	shader_desc.vs.source = "#version 330\n"
+							"uniform mat4 mvp;\n"
 							"layout(location=0) in vec4 position;\n"
 							"layout(location=1) in vec4 color0;\n"
 							"out vec4 color;\n"
 							"void main() {\n"
-							"  gl_Position = position;\n"
-							"  color = color0;\n"
+							"	gl_Position = mvp * position;\n"
+							"	color = color0;\n"
 							"}\n";
+
+	shader_desc.vs.uniform_blocks[0].size = sizeof(shader_vs_params);
+	shader_desc.vs.uniform_blocks[0].uniforms[0].name = "mvp";
+	shader_desc.vs.uniform_blocks[0].uniforms[0].type = SG_UNIFORMTYPE_MAT4;
+
 	shader_desc.fs.source = "#version 330\n"
 							"in vec4 color;\n"
 							"out vec4 frag_color;\n"
 							"void main() {\n"
-							"  frag_color = color;\n"
+							"	frag_color = color;\n"
 							"}\n";
 
 	sg_shader shd = sg_make_shader(&shader_desc);
@@ -79,24 +146,54 @@ void test_init_ref() {
 	sg_pass_action pass_action = {0};
 
 	// draw loop
-	while (!glfwWindowShouldClose(w)) {
+	shader_vs_params vs_params;
+
+	//
+	double old_xpos = 0, old_ypos = 0;
+	Vec3 pos(0, 0, 1), rot(0, 0, 0);
+
+	while (!glfwWindowShouldClose(win)) {
+		double xpos, ypos;
+		glfwGetCursorPos(win, &xpos, &ypos);
+		const double xdelta = xpos - old_xpos, ydelta = ypos - old_ypos;
+		old_xpos = xpos;
+		old_ypos = ypos;
+
+		fmt::print("dx={} dy={} up={} down={} left={} right={}\n", xdelta, ydelta, key_up, key_down, key_left, key_right);
+
+		//
+		FpsController(key_up, key_down, key_left, key_right, true, float(-xdelta), float(-ydelta), pos, rot, 2.f, time_from_ms(16));
+
+		const Mat44 proj = ComputePerspectiveProjectionMatrix(0.1f, 1000.f, 3.2f, ComputeAspectRatioX(640.f, 480.f));
+		const Mat4 view = InverseFast(TransformationMat4(pos, rot));
+
+		vs_params.mvp = Transpose(proj * view);
+
+		//
 		int cur_width, cur_height;
-		glfwGetFramebufferSize(w, &cur_width, &cur_height);
+		glfwGetFramebufferSize(win, &cur_width, &cur_height);
 		sg_begin_default_pass(&pass_action, cur_width, cur_height);
 		sg_apply_pipeline(pip);
 		sg_apply_bindings(&bind);
+
+		{
+			sg_range range;
+			range.ptr = &vs_params;
+			range.size = sizeof(vs_params);
+			sg_apply_uniforms(SG_SHADERSTAGE_VS, 0, range);
+		}
+
 		sg_draw(0, 3, 1);
 		sg_end_pass();
 		sg_commit();
-		glfwSwapBuffers(w);
+		glfwSwapBuffers(win);
 		glfwPollEvents();
 	}
 
-	// cleanup
-	sg_shutdown();
-	glfwTerminate();
+	RenderShutdown();
 }
 
+//
 TEST_LIST = {
 	{"Init", test_init},
 
