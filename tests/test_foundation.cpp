0967eb389b947b197e701e96f1d7e8d4ac8cfe45
MooZ <mooz@blockos.org>
2022-07-05 17:26:52 +0200

    Added more unit tests.

--- tests/test_foundation.cpp
+++ tests/test_foundation.cpp
@@ -260,35 +260,65 @@ void test_units() {
 	TEST_CHECK(TestEqual(RadianToDegree(Pi), 180.f));
 	TEST_CHECK(TestEqual(RadianToDegree(7.f * Pi / 4.f), 315.f));
 	TEST_CHECK(TestEqual(RadianToDegree(TwoPi), 360.f));
-		
-// [todo] template <typename T> constexpr T Sec(T v)
+	
+	TEST_CHECK(TestEqual(Csec(1.0f), 0.01f));
+	TEST_CHECK(TestEqual(Csec(100.f), 1.f));
 
-// [todo] template <typename T> constexpr T Csec(T v)
-// [todo] template <typename T> constexpr T Ms(T v)
+	TEST_CHECK(TestEqual(Ms(1.0f), 0.001f));
+	TEST_CHECK(TestEqual(Ms(1000.f), 1.f));
 
-// [todo] template <typename T> constexpr T Ton(T v) { return v * T(1000); }
-// [todo] template <typename T> constexpr T Kg(T v) { return v; }
+	TEST_CHECK(TestEqual(Ton(2.f), 2000.f));
+	TEST_CHECK(TestEqual(Ton(0.001f), 1.f));
 
-// [todo] template <typename T> constexpr T G(T v)
+	TEST_CHECK(TestEqual(Kg(2.f), 2.f));
 
-// [todo] template <typename T> constexpr T Km(T v)
-// [todo] template <typename T> constexpr T Mtr(T v)
+	TEST_CHECK(TestEqual(G(2.f), 0.002f));
+	TEST_CHECK(TestEqual(G(1000.f), 1.f));
 
-// [todo] template <typename T> constexpr T Cm(T v)
-// [todo] template <typename T> constexpr T Mm(T v)
-// [todo] template <typename T> constexpr T Inch(T v)
+	TEST_CHECK(TestEqual(Km(1.f), 1000.f));
+	TEST_CHECK(TestEqual(Km(0.001f), 1.f));
 
-// [todo] inline constexpr size_t KB(const size_t size)
-// [todo] inline constexpr size_t MB(const size_t size)
+	TEST_CHECK(TestEqual(Mtr(1.f), 1.f));
 
-// [todo] template <typename T> std::string FormatMemorySize(T v_)
+	TEST_CHECK(TestEqual(Cm(1.f), 0.01f));
+	TEST_CHECK(TestEqual(Cm(100.f), 1.f));
 
-// [todo] template <typename T> std::string FormatCount(T v_)
+	TEST_CHECK(TestEqual(Mm(1.f), 0.001f));
+	TEST_CHECK(TestEqual(Mm(1000.f), 1.f));
+	
+	TEST_CHECK(TestEqual(Inch(1.f), 0.0254f));
+	TEST_CHECK(TestEqual(Inch(39.3701f), 1.f));
+	
+	TEST_CHECK(TestEqual(KB(1), 1024));
+	TEST_CHECK(TestEqual(KB(1024), MB(1)));
 
-// [todo] template <typename T> std::string FormatDistance(T v_)
+	TEST_CHECK(FormatMemorySize(512) == "512B");
+	TEST_CHECK(FormatMemorySize(KB(1)) == "1.0KB");
+	TEST_CHECK(FormatMemorySize(MB(1)) == "1.0MB");
+	TEST_CHECK(FormatMemorySize(MB(1024)) == "1.0GB");
+	TEST_CHECK(FormatMemorySize(MB(1024*1024)) == "1.0TB");
+	TEST_CHECK(FormatMemorySize(KB(1) + 512) == "1.5KB");
+	TEST_CHECK(FormatMemorySize(MB(2048+768)) == "2.7GB");
+	TEST_CHECK(FormatMemorySize(MB(1) + KB(512)) == "1.5MB");
 
-// [todo] std::string FormatTime(time_ns t)
+	TEST_CHECK(FormatCount(1) == "1");
+	TEST_CHECK(FormatCount(10) == "10");
+	TEST_CHECK(FormatCount(1000) == "1.0K");
+	TEST_CHECK(FormatCount(12345) == "12.3K");
+	TEST_CHECK(FormatCount(2000000) == "2.0M");
+	TEST_CHECK(FormatCount(5555555) == "5.5M");
 
+	TEST_CHECK(FormatDistance(Mm(1.f)) == "1.0mm");
+	TEST_CHECK(FormatDistance(Mm(8.2f)) == "8.2mm");
+	TEST_CHECK(FormatDistance(Cm(1.f)) == "10.0mm");
+	TEST_CHECK(FormatDistance(Cm(53.5f)) == "53.5cm");
+	TEST_CHECK(FormatDistance(45.3f) == "45.3m");
+	TEST_CHECK(FormatDistance(Km(1.f)) == "1.0km");
+	TEST_CHECK(FormatDistance(Km(12.345)) == "12.3km");
+
+	TEST_CHECK(FormatTime(time_from_sec(1) + time_from_ms(100)) == "1 sec 100 ms");
+	TEST_CHECK(FormatTime(time_from_min(30) + time_from_sec(26) + time_from_ms(3)) == "30 min 26 sec 3 ms");
+	TEST_CHECK(FormatTime(time_from_hour(2) + time_from_min(15) + time_from_sec(30) + time_from_ms(222)) == "2 hour 15 min 30 sec 222 ms");
 }
 
 //
