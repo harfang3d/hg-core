0ab6f9dbd3195200b2470930ffee6817df45a222
MooZ <mooz@blockos.org>
2022-07-21 17:10:48 +0200

    Added anim unit tests.

---
+++ tests/engine/anim.cpp
@@ -0,0 +1,11 @@
+// HARFANG(R) Copyright (C) 2022 NWNC. Released under GPL/LGPL/Commercial Licence, see licence.txt for details.
+
+#define TEST_NO_MAIN
+#include "acutest.h"
+
+#include "engine/anim.h"
+
+using namespace hg;
+
+void test_anim() {
+}
