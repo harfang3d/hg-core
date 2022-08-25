bc1e66858b9fb7376d3e8dad24d8c64226ef7c9f
MooZ <mooz@blockos.org>
2022-08-25 17:49:31 +0200

    Minor cleanup.

--- tests/foundation/pack_float.cpp
+++ tests/foundation/pack_float.cpp
@@ -43,26 +43,3 @@ void test_pack_float() {
 	TEST_CHECK(pack_float<int16_t>(-0.8f, 2.f) == -13107);
 	TEST_CHECK(AlmostEqual(unpack_float<int16_t>(-13107, 2.f), -0.8f, 1.f / 32767.f));
 }
-
-/*
-
-
-template <typename T> inline T pack_float(float v, float scale = 1.F) {
-	return static_cast<T>(Clamp(v / scale, (std::is_signed<T>::value) ? -1.F : 0.F, 1.F) * std::numeric_limits<T>::max());
-}
-
-template <typename T> inline float unpack_float(T v, float scale = 1.F) {
-	return (static_cast<float>(v) / std::numeric_limits<T>::max()) * scale;
-}
-
-//
-template <> inline float pack_float<float>(float v, float) {
-	return v;
-}
-
-template <> inline float unpack_float<float>(float v, float) {
-	return v;
-}
-
-
-*/
